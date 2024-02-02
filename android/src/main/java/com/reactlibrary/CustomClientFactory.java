6cee7edb3247e0aef38f409c664bb27403d7b0d5
triracle97 <triracle@gmail.com>
2024-02-02 23:02:51 +0700

    Make android work

--- android/src/main/java/com/reactlibrary/CustomClientFactory.java
+++ android/src/main/java/com/reactlibrary/CustomClientFactory.java
@@ -8,10 +8,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.KeyManagerFactory;
@@ -27,7 +30,10 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.network.OkHttpClientFactory;
 import com.facebook.react.modules.network.ReactCookieJarContainer;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class CustomClientFactory implements OkHttpClientFactory {
     public static String certificateFileP12;
@@ -65,7 +71,7 @@ public class CustomClientFactory implements OkHttpClientFactory {
             };
             SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
             sslContext.init(keyManagerFactory.getKeyManagers(), new TrustManager[]{ trustManager }, new SecureRandom());
-            return new OkHttpClient.Builder()
+            OkHttpClient.Builder builder = new OkHttpClient.Builder()
                     .sslSocketFactory(sslContext.getSocketFactory(), trustManager)
                     .hostnameVerifier(new HostnameVerifier() {
                         @Override
@@ -73,12 +79,49 @@ public class CustomClientFactory implements OkHttpClientFactory {
                             return true;
                         }
                     })
-                    .cookieJar(new ReactCookieJarContainer()).build();
+                    .cookieJar(new ReactCookieJarContainer());
+
+            builder.addInterceptor(new CustomInterceptor());
+
+            return builder.build();
 
         } catch (
                 Exception e) {
             Log.e(TAG, e.getMessage());
             throw new RuntimeException(e);
+        }
+    }
+
+    private static class CustomInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request originalRequest = chain.request();
+
+            // Log request headers
+            Map<String, String> uppercaseHeaders = convertHeadersToUppercase(originalRequest.headers());
+
+            okhttp3.Headers.Builder headersBuilder = new okhttp3.Headers.Builder();
+            for (Map.Entry<String, String> entry : uppercaseHeaders.entrySet()) {
+                headersBuilder.add(entry.getKey(), entry.getValue());
+            }
+
+            Request newRequest = originalRequest.newBuilder()
+                    .headers(headersBuilder.build())
+                    .build();
+
+            Response response = chain.proceed(newRequest);
+
+            return response;
+        }
+
+        private Map<String, String> convertHeadersToUppercase(okhttp3.Headers headers) {
+            Map<String, String> uppercaseHeaders = new HashMap<>();
+            for (int i = 0, size = headers.size(); i < size; i++) {
+                String key = headers.name(i).toUpperCase();
+                String value = headers.value(i);
+                uppercaseHeaders.put(key, value);
+            }
+            return uppercaseHeaders;
         }
     }
 }
